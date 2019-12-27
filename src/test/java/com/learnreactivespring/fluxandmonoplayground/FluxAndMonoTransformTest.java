e44e17407cc438d507030eeb61fbfb0db7944923
vlasisPi <vlassisp89@gmail.com>
2019-12-27 17:42:41 +0200

    Reactive streams using map

---
+++ src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTransformTest.java
@@ -0,0 +1,59 @@
+package com.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FluxAndMonoTransformTest {
+
+    List<String> names = Arrays.asList("adam", "anna", "jack", "jenny");
+
+    @Test
+    public void transformUsingMap() {
+        Flux<String> namesFlux = Flux.fromIterable(names)
+                .map(s -> s.toUpperCase())
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("ADAM", "ANNA", "JACK", "JENNY")
+                .verifyComplete();
+    }
+
+    @Test
+    public void transformUsingMap_Length() {
+        Flux<Integer> namesFlux = Flux.fromIterable(names)
+                .map(s -> s.length())
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext(4, 4, 4, 5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void transformUsingMap_Length_repeat() {
+        Flux<Integer> namesFlux = Flux.fromIterable(names)
+                .map(s -> s.length())
+                .repeat(1)
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext(4, 4, 4, 5, 4, 4, 4, 5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void transformUsingMap_Filter() {
+        Flux<String> namesFlux = Flux.fromIterable(names)
+                .filter(s -> s.length() > 4)
+                .map(s -> s.toUpperCase())
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("JENNY")
+                .verifyComplete();
+    }
+}
