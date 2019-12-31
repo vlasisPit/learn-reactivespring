9b583fb0f6caeac11708808934a112711755286c
vlasisPi <vlassisp89@gmail.com>
2019-12-31 20:06:28 +0200

    Build simple non-blocking API

---
+++ src/main/java/com/learnreactivespring/controller/FluxAndMonoController.java
@@ -0,0 +1,33 @@
+package com.learnreactivespring.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+@RestController
+public class FluxAndMonoController {
+
+    /**
+     * Returns by default a Json and the browser is a blocking client
+     * That's why it waits for 4 sec and then render on the browser the result which is
+     * [1,2,3,4]
+     * @return
+     */
+    @GetMapping("/flux")
+    public Flux<Integer> returnFlux() {
+        return Flux.just(1,2,3,4)
+                .delayElements(Duration.ofSeconds(1))
+                .log();
+    }
+
+    @GetMapping(value = "/fluxstream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Integer> returnFluxStream() {
+        return Flux.just(1,2,3,4)
+                .delayElements(Duration.ofSeconds(1))
+                .log();
+    }
+
+}
