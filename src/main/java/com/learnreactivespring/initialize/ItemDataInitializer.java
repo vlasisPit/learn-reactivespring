4630b5bacba1ac7fc46c63b252b127c36097cd73
vlasisPi <vlassisp89@gmail.com>
2020-01-25 18:29:31 +0200

    Initialize the data using CommandLineRunner

---
+++ src/main/java/com/learnreactivespring/initialize/ItemDataInitializer.java
@@ -0,0 +1,43 @@
+package com.learnreactivespring.initialize;
+
+import com.learnreactivespring.document.Item;
+import com.learnreactivespring.repository.ItemReactiveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Set this class to Component in order to be scanned during the application start up
+ */
+@Component
+public class ItemDataInitializer implements CommandLineRunner {
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        initialDataSetUp();
+    }
+
+    private void initialDataSetUp() {
+        itemReactiveRepository.deleteAll()      //delete all the data from MongoDB
+                .thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .thenMany(itemReactiveRepository.findAll())
+                .subscribe(item -> System.out.println("Item inserted from CommandLineRunner : " + item));       //NEVER block here, only subscribe
+    }
+
+    private List<Item> data() {
+        return Arrays.asList(
+                new Item(null, "Samsung TV", 399.99),
+                new Item(null, "LG TV", 329.99),
+                new Item(null, "Apple Watch", 349.99),
+                new Item(null, "Beats HeadPhones", 19.99)
+        );
+    }
+}
