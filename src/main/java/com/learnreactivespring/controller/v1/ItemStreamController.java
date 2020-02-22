ba670a37390d18b03e61afc339b14acae8d9a573
vlasisPi <vlassisp89@gmail.com>
2020-02-22 14:33:39 +0200

    Instructions about how to access streaming endpoint with curl

--- src/main/java/com/learnreactivespring/controller/v1/ItemStreamController.java
+++ src/main/java/com/learnreactivespring/controller/v1/ItemStreamController.java
@@ -21,6 +21,14 @@ public class ItemStreamController {
     /**
      * Use APPLICATION_STREAM_JSON_VALUE, if you want to stream the response
      * Run this from browser http://localhost:8080/v1/stream/items
+     * Use  curl --no-buffer http://localhost:8080/v1/stream/items
+     * to access the data from curl. Browser does not buffer data.
+     * From https://curl.haxx.se/docs/manpage.html ...
+     * -N, --no-buffer
+     * Disables the buffering of the output stream. In normal work situations, curl will use a standard buffered output
+     * stream that will have the effect that it will output the data in chunks, not necessarily exactly when the data
+     * arrives. Using this option will disable that buffering.
+     * I use this option to process data while it arrives, so I don't have to wait until the request completes
      */
     @GetMapping(value = ITEM_STREAM_END_POINT_V1, produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<ItemCapped> getItemsStream() {
